4c0a83c4c10552b8855eef9d9e5ce712cf15d501
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2023-01-03 22:17:13 -0300

    Add certificates

--- src/Components/Views/Studies/Certificates.jsx
+++ src/Components/Views/Studies/Certificates.jsx
@@ -1,19 +1,5 @@
 import React from "react";
-import styled from "styled-components";
 import images from "../../../assets/images";
-
-const Container = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--background-color);
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  border: aliceblue 2px solid;
-`;
 
 const Title = styled.h1`
   text-align: center;
@@ -27,74 +13,31 @@ const Certificates = () => {
 
   return (
     <>
-      <Container id="certificates">
+      <div id="certificates">
         <Title>Certificates</Title>
-        <div className="certificate">
-          <img src={alkemy} className="certificate_img"></img>
-          <div className="certificate_info">
+        <div class="grid-certificates">
+          <div className="certificate">
             <h2>Alkemy Job Ready React.js </h2>
-            <p>
-              Sea et sea sea magna dolore consetetur sea at amet consequat amet
-              dolor lorem diam rebum labore elitr velit sed liber eum est
-              accusam justo consetetur diam stet at dolor sed praesent exerci
-              placerat kasd illum dolor eros iriure lorem no nulla sanctus
-              takimata vero tincidunt volutpat dolor laoreet consetetur et
-            </p>
+            <img src={alkemy} className="certificate_img" alt=""></img>
           </div>
-        </div>
-        <div className="certificate">
-          <img src={iters} className="certificate_img"></img>
-          <div className="certificate_info">
+          <div className="certificate">
             <h2>Full stack Course (Iters)</h2>
-            <p>
-              Sea et sea sea magna dolore consetetur sea at amet consequat amet
-              dolor lorem diam rebum labore elitr velit sed liber eum est
-              accusam justo consetetur diam stet at dolor sed praesent exerci
-              placerat kasd illum dolor eros iriure lorem no nulla sanctus
-              takimata vero tincidunt volutpat dolor laoreet consetetur et
-            </p>
+            <img src={iters} className="certificate_img" alt=""></img>
           </div>
-        </div>
-        <div className="certificate">
-          <img src={utn} className="certificate_img"></img>
-          <div className="certificate_info">
+          <div className="certificate">
             <h2>JavaScript Course (UTN)</h2>
-            <p>
-              Sea et sea sea magna dolore consetetur sea at amet consequat amet
-              dolor lorem diam rebum labore elitr velit sed liber eum est
-              accusam justo consetetur diam stet at dolor sed praesent exerci
-              placerat kasd illum dolor eros iriure lorem no nulla sanctus
-              takimata vero tincidunt volutpat dolor laoreet consetetur et
-            </p>
+            <img src={utn} className="certificate_img" alt=""></img>
           </div>
-        </div>
-        <div className="certificate">
-          <img src={node} className="certificate_img"></img>
-          <div className="certificate_info">
+          <div className="certificate">
             <h2>Node.js (Udemy)</h2>
-            <p>
-              Sea et sea sea magna dolore consetetur sea at amet consequat amet
-              dolor lorem diam rebum labore elitr velit sed liber eum est
-              accusam justo consetetur diam stet at dolor sed praesent exerci
-              placerat kasd illum dolor eros iriure lorem no nulla sanctus
-              takimata vero tincidunt volutpat dolor laoreet consetetur et
-            </p>
+            <img src={node} className="certificate_img" alt=""></img>
           </div>
-        </div>
-        <div className="certificate">
-          <img src={udejs} className="certificate_img"></img>
-          <div className="certificate_info">
+          <div className="certificate">
             <h2>JavaScript Course (Udemy)</h2>
-            <p>
-              Sea et sea sea magna dolore consetetur sea at amet consequat amet
-              dolor lorem diam rebum labore elitr velit sed liber eum est
-              accusam justo consetetur diam stet at dolor sed praesent exerci
-              placerat kasd illum dolor eros iriure lorem no nulla sanctus
-              takimata vero tincidunt volutpat dolor laoreet consetetur et
-            </p>
+            <img src={udejs} className="certificate_img" alt=""></img>
           </div>
         </div>
-      </Container>
+      </div>
     </>
   );
 };
